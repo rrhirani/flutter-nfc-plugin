97546f58cca0c8efeb985bf23d40bc5f0cf89d27
Ravi <rrhiraniphp@gmail.com>
2021-12-12 18:38:09 +1300

    p1 - v2 changes

--- example/android/app/src/main/kotlin/it/dukhanov/flutter/nfc_plugin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/it/dukhanov/flutter/nfc_plugin_example/MainActivity.kt
@@ -1,16 +1,7 @@
 package it.dukhanov.flutter.nfc_plugin_example
 
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    Log.w("flutterNfc", "onCreate")
-    GeneratedPluginRegistrant.registerWith(this)
-  }
+
 }
